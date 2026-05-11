a4d60bdfd005d450650fa3d19e38149bd98f69fd
Menta87 <robert.ionita1987@gmail.com>
2026-05-11 13:14:24 +0300

    Adaug Variantele 10, 11, 12 - Teste 15, 16, 17 BAC 2020

---
+++ src/data/bac/varianta-12.js
@@ -0,0 +1,114 @@
+export const varianta12 = {
+  id: 'varianta-12',
+  nume: 'Varianta 12 - Test 17 BAC 2020',
+  an: 2020,
+  totalPuncte: 90,
+  
+  subiectI: {
+    titlu: 'Subiect I - Europa',
+    harta: 'Test_6',
+    
+    A: [
+      { tip: 'identificare', enunt: '1. Numele statului marcat cu litera J', raspunsCorect: 'Spania', raspunsuriAcceptate: ['spania', 'Spania'], punctaj: 2 },
+      { tip: 'identificare', enunt: '2. Numele orașului-capitală marcat cu numărul 7', raspunsCorect: 'Chișinău', raspunsuriAcceptate: ['chisinau', 'Chișinău', 'Chisinau'], punctaj: 2 },
+    ],
+    
+    B: [
+      { tip: 'completare', enuntInainte: '1. Orașul-capitală Reykjavik este marcat cu numărul', raspunsCorect: '10', raspunsuriAcceptate: ['10'], punctaj: 2 },
+      { tip: 'completare', enuntInainte: '2. Cel mai mic stat din Europa este o enclavă pe teritoriul statului marcat cu litera', raspunsCorect: 'F', raspunsuriAcceptate: ['F', 'f', 'Italia', 'italia'], punctaj: 2 },
+      { tip: 'completare', enuntInainte: '3. Capitala statului marcat cu litera G se numește', raspunsCorect: 'Londra', raspunsuriAcceptate: ['londra', 'Londra', 'London'], punctaj: 2 },
+    ],
+    
+    C: [
+      { tip: 'grila', enunt: '1. Capitala statului marcat cu litera H se numește:', variante: { a: 'Riga', b: 'Tallinn', c: 'Varșovia', d: 'Vilnius' }, raspunsCorect: 'd', punctaj: 2 },
+      { tip: 'grila', enunt: '2. Pe teritoriul statului marcat cu litera F se află Munții:', variante: { a: 'Alpii Scandinaviei', b: 'Alpii Dinarici', c: 'Apenini', d: 'Pirinei' }, raspunsCorect: 'c', punctaj: 2 },
+      { tip: 'grila', enunt: '3. Orașul marcat cu numărul 13 este capitala statului:', variante: { a: 'Albania', b: 'Bosnia și Herțegovina', c: 'Macedonia de Nord', d: 'Muntenegru' }, raspunsCorect: 'a', punctaj: 2 },
+      { tip: 'grila', enunt: '4. Cel mai mare port fluvial al Europei este situat în statul marcat cu litera:', variante: { a: 'B', b: 'E', c: 'I', d: 'J' }, raspunsCorect: 'c', punctaj: 2 },
+      { tip: 'grila', enunt: '5. Câmpia Panonică se află pe teritoriul statului marcat cu litera:', variante: { a: 'A', b: 'B', c: 'D', d: 'F' }, raspunsCorect: 'a', punctaj: 2 },
+    ],
+    
+    D: [
+      { tip: 'comparatie', enunt: 'Precizați 3 deosebiri între clima statului C (Portugalia) și clima statului E (Norvegia)', raspunsExemplu: [
+        'Tip de climă: Portugalia are climă mediteraneană, iar Norvegia are climă subpolară oceanică',
+        'Temperaturi: Portugalia are temperaturi blânde (10-25°C), iar Norvegia scăzute (-5 până la 15°C)',
+        'Precipitații: Portugalia are precipitații concentrate iarna (700-1000 mm), iar Norvegia uniforme tot anul (1500-2500 mm)',
+      ], punctajMaxim: 6 },
+    ],
+    
+    E: [
+      { tip: 'cauza', enunt: '1. Explicați faptul că majoritatea statelor europene înregistrează bilanț natural negativ', raspunsExemplu: 'Natalitate scăzută datorită schimbării mentalității (familii cu un copil), urbanizării, costurilor mari de creștere, dar și mortalitate ridicată din cauza îmbătrânirii populației.', cuvinteCheie: ['natalitate', 'îmbătrânire', 'urbanizare'], punctaj: 2 },
+      { tip: 'cauza', enunt: '2. Explicați ponderea ridicată a energiei termice convenționale în Rusia', raspunsExemplu: 'Rusia dispune de zăcăminte uriașe de gaz natural (cele mai mari din lume), petrol și cărbune, fiind cel mai mare producător/exportator de hidrocarburi. Tradiție industrială în energia termoelectrică.', cuvinteCheie: ['gaz', 'petrol', 'cărbune', 'hidrocarburi'], punctaj: 2 },
+    ],
+  },
+  
+  subiectII: {
+    titlu: 'Subiect II - România',
+    harta: 'Test_6',
+    
+    A: [
+      { tip: 'identificare', enunt: '1. Numele orașului marcat cu numărul 8', raspunsCorect: 'Constanța', raspunsuriAcceptate: ['constanta', 'Constanța', 'Constanta'], punctaj: 2 },
+      { tip: 'identificare', enunt: '2. Numele râului marcat cu numărul 5', raspunsCorect: 'Bistrița', raspunsuriAcceptate: ['bistrita', 'Bistrița', 'Bistrita'], punctaj: 2 },
+    ],
+    
+    B: [
+      { tip: 'completare', enuntInainte: '1. Orașul marcat cu numărul 9 este străbătut de râul numit', raspunsCorect: 'Mureș', raspunsuriAcceptate: ['mures', 'Mureș', 'Mures'], punctaj: 2 },
+      { tip: 'completare', enuntInainte: '2. Orașul Drobeta-Turnu Severin este marcat cu numărul', raspunsCorect: '11', raspunsuriAcceptate: ['11'], punctaj: 2 },
+      { tip: 'completare', enuntInainte: '3. Râul marcat cu numărul 6 se numește', raspunsCorect: 'Someș', raspunsuriAcceptate: ['somes', 'Someș', 'Somes'], punctaj: 2 },
+    ],
+    
+    C: [
+      { tip: 'grila', enunt: '1. Peșteri și chei se găsesc în unitatea de relief marcată cu litera:', variante: { a: 'B', b: 'D', c: 'F', d: 'H' }, raspunsCorect: 'b', punctaj: 2 },
+      { tip: 'grila', enunt: '2. Orașul marcat cu numărul 12 este străbătut de:', variante: { a: 'Jiu', b: 'Lotru', c: 'Motru', d: 'Olt' }, raspunsCorect: 'a', punctaj: 2 },
+      { tip: 'grila', enunt: '3. Vegetația alpină este bine reprezentată în unitatea de relief marcată cu litera:', variante: { a: 'E', b: 'F', c: 'G', d: 'H' }, raspunsCorect: 'c', punctaj: 2 },
+      { tip: 'grila', enunt: '4. Râul marcat cu numărul 3 se numește:', variante: { a: 'Bârlad', b: 'Buzău', c: 'Siret', d: 'Trotuș' }, raspunsCorect: 'a', punctaj: 2 },
+      { tip: 'grila', enunt: '5. Munți vulcanici se găsesc în unitatea de relief marcată cu litera:', variante: { a: 'A', b: 'C', c: 'E', d: 'G' }, raspunsCorect: 'a', punctaj: 2 },
+    ],
+    
+    D: [
+      { tip: 'comparatie', enunt: 'Precizați 3 deosebiri între relieful unității G (Grupa Parâng) și unității H (Câmpia Transilvaniei)', raspunsExemplu: [
+        'Mod de formare: Parâng - orogeneza alpină (cutare), Câmpia Transilvaniei - sedimentare în bazin intramontan',
+        'Altitudini: Parâng - 2519 m (Vârful Parângu Mare), Câmpia Transilvaniei - 400-500 m',
+        'Roci: Parâng - magmatice, metamorfice, calcare; Câmpia Transilvaniei - argile, marne, sare',
+      ], punctajMaxim: 6 },
+    ],
+    
+    E: [
+      { tip: 'cauza', enunt: '1. Prezentați o cauză a debitului scăzut al râului marcat cu numărul 2 (Ialomița sau Argeș)', raspunsExemplu: 'Râul izvorăște din zone cu altitudini mai mici, traversează regiuni cu precipitații reduse (400-600 mm/an), are bazin hidrografic relativ mic și puține râuri afluente mari.', cuvinteCheie: ['izvor', 'precipitații', 'bazin'], punctaj: 2 },
+      { tip: 'cauza', enunt: '2. Prezentați o altă cauză', raspunsExemplu: 'Climă temperat-continentală cu evaporație ridicată vara, prezența numeroaselor amenajări (baraje) care reglează și diminuează debitul natural, captări pentru consum și agricultură.', cuvinteCheie: ['evaporație', 'baraje', 'captări'], punctaj: 2 },
+    ],
+  },
+  
+  subiectIII: {
+    titlu: 'Subiect III - Lumea contemporană',
+    
+    A: [
+      { tip: 'identificare', enunt: '1. Valoarea maximă a temperaturii medii lunare la Kiev (între 19 și 19,8°C) și luna (iulie)', raspunsCorect: '19.5 iulie', raspunsuriAcceptate: ['19.5 iulie', 'iulie', '19 iulie', '20 iulie', '19', 'iulie 19'], punctaj: 2 },
+      { tip: 'identificare', enunt: '2. Valoarea minimă a temperaturii medii lunare la Kiev (-6°C) și luna (ianuarie)', raspunsCorect: '-6 ianuarie', raspunsuriAcceptate: ['-6 ianuarie', 'ianuarie', '-6'], punctaj: 2 },
+    ],
+    
+    B: [
+      { tip: 'calcul', enunt: '1. Calculați amplitudinea termică medie anuală (între 25 și 25,8°C)', formula: 'T max - T min', raspunsCorect: 25.5, intervalAcceptat: [25, 26], unitate: '°C', punctaj: 2 },
+      { tip: 'cauza', enunt: '2. Explicați valoarea ridicată a amplitudinii termice medii anuale', raspunsExemplu: 'Kiev este situat departe de mări/oceane (continentalitate), influențat de mase de aer continentale: vara cald (anticicloni continentali), iarna foarte rece (mase polare).', cuvinteCheie: ['continental', 'departe', 'anticicloni'], punctaj: 2 },
+      { tip: 'completare', enuntInainte: '3. Tipul de climă în care se află Kiev este', raspunsCorect: 'temperat continental', raspunsuriAcceptate: ['temperat continental', 'continental', 'temperat-continental', 'continental excesiv'], punctaj: 2 },
+    ],
+    
+    C: [
+      { tip: 'completare', enuntInainte: '1. Pentru Masivul Dobrogei de Nord: o orogeneză este', raspunsCorect: 'caledoniană', raspunsuriAcceptate: ['caledoniană', 'caledonica', 'hercinică', 'hercinica', 'kimerică', 'alpină'], punctaj: 2 },
+      { tip: 'completare', enuntInainte: '2. Pentru Masivul Dobrogei de Nord: un tip de rocă este', raspunsCorect: 'sisturi', raspunsuriAcceptate: ['sisturi', 'sisturi cristaline', 'calcar', 'calcare', 'granite', 'magmatice'], punctaj: 2 },
+      { tip: 'completare', enuntInainte: '3. Pentru Masivul Dobrogei de Nord: o subdiviziune este', raspunsCorect: 'Munții Măcin', raspunsuriAcceptate: ['Munții Măcin', 'macin', 'Podișul Babadag', 'Podișul Niculițel'], punctaj: 2 },
+      { tip: 'completare', enuntInainte: '4. Pentru Masivul Dobrogei de Nord: o influență climatică este', raspunsCorect: 'pontică', raspunsuriAcceptate: ['pontică', 'pontice', 'submediteraneană', 'continentală'], punctaj: 2 },
+      { tip: 'completare', enuntInainte: '5. Pentru Masivul Dobrogei de Nord: cel mai mare oraș este', raspunsCorect: 'Tulcea', raspunsuriAcceptate: ['Tulcea', 'tulcea'], punctaj: 2 },
+    ],
+    
+    D: [
+      { tip: 'identificare', enunt: '1. Statul european cu cea mai mare pondere a sectorului terțiar', raspunsCorect: 'Franța', raspunsuriAcceptate: ['Franța', 'franta', 'Franta'], punctaj: 2 },
+      { tip: 'identificare', enunt: '2. Statul european cu cea mai mică pondere a sectorului terțiar', raspunsCorect: 'Bulgaria', raspunsuriAcceptate: ['Bulgaria', 'bulgaria'], punctaj: 2 },
+      { tip: 'identificare', enunt: '3. Statul european cu cea mai mică pondere în sectorul primar', raspunsCorect: 'Slovenia', raspunsuriAcceptate: ['Slovenia', 'slovenia'], punctaj: 2 },
+    ],
+    
+    E: [
+      { tip: 'cauza', enunt: '1. Prezentați o cauză a ponderii ridicate a populației ocupate în sectorul primar în Bulgaria', raspunsExemplu: 'Bulgaria are economie agricolă tradițională, suprafețe mari de teren agricol, dezvoltare industrială mai redusă comparativ cu Vestul Europei, multe gospodării rurale de subzistență.', cuvinteCheie: ['agricolă', 'tradițional', 'rural', 'subzistență'], punctaj: 2 },
+      { tip: 'cauza', enunt: '2. Prezentați o cauză a ponderii scăzute a populației ocupate în sectorul primar în Franța', raspunsExemplu: 'Franța are agricultură intensiv mecanizată care necesită puțini lucrători, urbanizare mare (peste 80%), economie dezvoltată pe servicii și industrie, și politici UE de modernizare agricolă.', cuvinteCheie: ['mecanizare', 'urbanizare', 'servicii', 'modernizare'], punctaj: 2 },
+    ],
+  },
+};
